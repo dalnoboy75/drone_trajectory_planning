c968bb307e2f853bc9f383e447b6941b61cecc9c
Matvey <bolshakov.ma@phystech.edu>
2024-09-11 19:56:26 +0300

    Zero and First steps

---
+++ TSP.py
@@ -0,0 +1,44 @@
+import numpy as np
+from itertools import permutations
+
+
+def find_min_route(numbers, lines, columns):
+    # Считываем числа от пользователя
+    # numbers = input("Введите числа, разделенные пробелами: ")
+
+    # Преобразуем строку в список чисел
+    number_list = [np.inf if x == 1e9 else int(x) for x in numbers]
+
+    # Определяем размерность матрицы
+    # lines = int(input("Введите количество строк: "))
+    # columns = int(input("Введите количество столбцов: "))
+
+    # Проверяем, достаточно ли чисел для создания матрицы
+    if len(number_list) != lines * columns:
+        return "Ошибка: количество чисел не соответствует размерности матрицы."
+
+    # Создаем матрицу
+    matrix = np.array(number_list).reshape(columns, lines)
+
+    x = list(range(1, lines + 1))
+    number_solutions = list(permutations(x))
+    if len(number_solutions) == 1:  # проверка на пустоту списка
+        return "Нет циклов"
+
+    route_data = []
+
+    for i in number_solutions:
+        sum_length = 0
+        for j in range(len(i)):
+            if j == len(i) - 1:
+                sum_length += matrix[i[j] - 1][i[0] - 1]
+            else:
+                sum_length += matrix[i[j] - 1][i[j + 1] - 1]
+        route_data.append((sum_length, i))
+
+    return min(route_data)
+
+
+# Использование функции
+# result = find_min_route([], 0, 0)
+# print(result)
---
+++ test_find_min_route.py
@@ -0,0 +1,36 @@
+import pytest
+import numpy as np
+from TSP import find_min_route
+
+
+def test_valid_case():
+    numbers = [1e9, 5, 7, 16, 1e9, 2, 4, 6, 1e9]
+    lines = 3
+    columns = 3
+    expected = (11, (1, 2, 3))  # пример ожидаемого результата
+    assert find_min_route(numbers, lines, columns) == expected
+
+
+def test_invalid_matrix_size():
+    numbers = [0, 10, 15]
+    lines = 2
+    columns = 2
+    expected = "Ошибка: количество чисел не соответствует размерности матрицы."
+    assert find_min_route(numbers, lines, columns) == expected
+
+
+#
+def test_empty_permutations():
+    numbers = []
+    lines = 0
+    columns = 0
+    expected = "Нет циклов"
+    assert find_min_route(numbers, lines, columns) == expected
+
+
+def test_inf_value():
+    numbers = [1e9, 5, 16, 14, 13, 1e9, 6, 9, 10, 12, 1e9, 11, 8, 15, 7, 1e9]
+    lines = 4
+    columns = 4
+    expected = (30, (1, 2, 3, 4))  # пример ожидаемого результата
+    assert find_min_route(numbers, lines, columns) == expected
