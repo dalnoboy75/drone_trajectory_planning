74153667b3cf0195e1ade9f9fc9fc418bd06c20a
Matvey <bolshakov.ma@phystech.edu>
2024-09-18 20:00:55 +0300

    End task of traveling salesman

--- TSP.py
+++ TSP.py
@@ -2,26 +2,25 @@ import numpy as np
 from itertools import permutations
 
 
-def find_min_route(numbers, lines, columns):
-    # Считываем числа от пользователя
-    # numbers = input("Введите числа, разделенные пробелами: ")
-
-    # Преобразуем строку в список чисел
-    number_list = [np.inf if x == 1e9 else int(x) for x in numbers]
-
-    # Определяем размерность матрицы
-    # lines = int(input("Введите количество строк: "))
-    # columns = int(input("Введите количество столбцов: "))
-
-    # Проверяем, достаточно ли чисел для создания матрицы
-    if len(number_list) != lines * columns:
-        return "Ошибка: количество чисел не соответствует размерности матрицы."
-
-    # Создаем матрицу
-    matrix = np.array(number_list).reshape(columns, lines)
+def find_min_route(numbers):
+    matrix = np.array(numbers).reshape(int(len(numbers)**0.5), int(len(numbers)**0.5))
+    lines = int(len(numbers)**0.5)
+    columns = int(len(numbers) ** 0.5)
 
     x = list(range(1, lines + 1))
     number_solutions = list(permutations(x))
+    if len(number_solutions) == 1:  # проверка на пустоту списка
+        return "Нет циклов"
+
+    # Нахождение оценки снизу
+    result = np.array(numbers).reshape(int(len(numbers)**0.5), int(len(numbers)**0.5))
+    row_mins = matrix.min(axis=1)
+    result -= row_mins[:, np.newaxis]
+    columns_mins = result.min(axis=0)
+    result -= columns_mins
+    low_mark = sum(row_mins) + sum(columns_mins)
+
+
     if len(number_solutions) == 1:  # проверка на пустоту списка
         return "Нет циклов"
 
@@ -36,8 +35,14 @@ def find_min_route(numbers, lines, columns):
                 sum_length += matrix[i[j] - 1][i[j + 1] - 1]
         route_data.append((sum_length, i))
 
-    return min(route_data)
+    return min(route_data), low_mark
 
 
+# find_min_route([10**8, 5, 16, 14, 13, 10**8, 6, 9, 10, 12, 10**8, 11, 8, 15, 7, 10**8])
 # result = find_min_route([], 0, 0)
 # print(result)
+#
+#print(find_min_route([10**8, 5, 16, 14, 13, 10**8, 6, 9, 10, 12, 10**8, 11, 8, 15, 7, 10**8]))
+# print(find_min_route([10**8, 1, 2, 3, 4, 14, 10**8, 15, 16, 5, 13, 20, 10**8, 17, 6, 12, 19, 18, 10**8, 7, 11, 10, 9, 8, 10**8]))
+# print(find_min_route([10**8, 5, 7, 16, 10**8, 2, 4, 6, 10**8]))
+# print(find_min_route([]))
--- test_find_min_route.py
+++ test_find_min_route.py
@@ -4,33 +4,23 @@ from TSP import find_min_route
 
 
 def test_valid_case():
-    numbers = [1e9, 5, 7, 16, 1e9, 2, 4, 6, 1e9]
-    lines = 3
-    columns = 3
-    expected = (11, (1, 2, 3))
-    assert find_min_route(numbers, lines, columns) == expected
-
-
-def test_invalid_matrix_size():
-    numbers = [0, 10, 15]
-    lines = 2
-    columns = 2
-    expected = "Ошибка: количество чисел не соответствует размерности матрицы."
-    assert find_min_route(numbers, lines, columns) == expected
-
+    numbers = [10**8, 5, 7, 16, 10**8, 2, 4, 6, 10**8]
+    expected = ((11, (1, 2, 3)), 11)
+    assert find_min_route(numbers) == expected
 
 #
 def test_empty_permutations():
     numbers = []
-    lines = 0
-    columns = 0
     expected = "Нет циклов"
-    assert find_min_route(numbers, lines, columns) == expected
+    assert find_min_route(numbers) == expected
 
 
 def test_inf_value():
-    numbers = [1e9, 5, 16, 14, 13, 1e9, 6, 9, 10, 12, 1e9, 11, 8, 15, 7, 1e9]
-    lines = 4
-    columns = 4
-    expected = (30, (1, 2, 3, 4))  # пример ожидаемого результата
-    assert find_min_route(numbers, lines, columns) == expected
+    numbers = [10**8, 5, 16, 14, 13, 10**8, 6, 9, 10, 12, 10**8, 11, 8, 15, 7, 10**8]
+    expected = ((30, (1, 2, 3, 4)), 29)  # пример ожидаемого результата
+    assert find_min_route(numbers) == expected
+
+def test_inf_value_1():
+    numbers = [10**8, 1, 2, 3, 4, 14, 10**8, 15, 16, 5, 13, 20, 10**8, 17, 6, 12, 19, 18, 10**8, 7, 11, 10, 9, 8, 10**8]
+    expected = ((42, (1, 2, 3, 5, 4)), 31)  # пример ожидаемого результата
+    assert find_min_route(numbers) == expected
