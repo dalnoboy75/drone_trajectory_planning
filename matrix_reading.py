155b1179271dbce04660d5575bd76b7f55ececf5
Ivan Novikov <novikovivan005@gmail.com>
2024-09-18 20:16:39 +0300

    add forbidden segments and rewrite tests

--- matrix_reading.py
+++ matrix_reading.py
@@ -2,9 +2,13 @@ import json
 import numpy as np
 
 
-def matrix_reading(data_path):
-    with open(data_path) as file:
+def reading_matrix():
+    with open('data.json') as file:
         data = json.load(file)
+    matrix_distance(data)
+
+
+def matrix_distance(data: dict):
     try:
         points = np.array([(float(point['x']), float(point['y'])) for point in data['points']])
         point_amount = len(points)
@@ -12,10 +16,14 @@ def matrix_reading(data_path):
         distance_matrix = np.sqrt(np.sum((points[:, np.newaxis] - points[np.newaxis, :]) ** 2, axis=-1))
         distance_matrix = np.round(distance_matrix, 6)
         np.fill_diagonal(distance_matrix, 10 ** 8)
+        for segment in data['forbid_segments']:
+            start = segment[0]
+            finish = segment[1]
+            distance_matrix[start, finish] = distance_matrix[finish, start] = 10**8
         return distance_matrix
     except:
         return 'error'
 
 
 if __name__ == '__main__':
-    pass
+    reading_matrix()
--- tests.py
+++ tests.py
@@ -1,83 +1,67 @@
 import pytest
 import numpy as np
 import json
-from matrix_reading import matrix_reading
+from matrix_reading import matrix_distance
 
-mock_data = {
+mock_data_1 = {
     "points": [
         {"x": 0.5, "y": 0},
         {"x": 3, "y": 4.2},
         {"x": 6.3, "y": -8}
+    ],
+    "forbid_segments": [
+
     ]
 }
-
-@pytest.fixture
-def setup_data_file(tmp_path):
-    data_file = tmp_path / "data.json"
-    with open(data_file, 'w') as f:
-        json.dump(mock_data, f)
-    return data_file
-
-def test_matrix_reading(setup_data_file):
-    distance_matrix = matrix_reading(setup_data_file)
-    expected_matrix = np.array([
-        [10**8, 4.887740, 9.881295],
-        [4.88774, 10**8, 12.638433],
-        [9.881295, 12.638433, 10**8]
-    ])
-
-    np.testing.assert_array_almost_equal(distance_matrix, expected_matrix)
-
-mock_empty_data = {
-    "points": []
-}
+expected_matrix_1 = np.array([
+    [10 ** 8, 4.887740, 9.881295],
+    [4.88774, 10 ** 8, 12.638433],
+    [9.881295, 12.638433, 10 ** 8]
+])
 
 mock_single_point_data = {
-    "points": [{"x": 1, "y": 1}]
+    "points": [{"x": 1, "y": 1}],
+    "forbid_segments": [
+
+    ]
 }
+expected_matrix_2 = np.array([[10 ** 8]])
 
 mock_two_points_data = {
     "points": [
         {"x": 1, "y": 1},
         {"x": 4, "y": 5}
+    ],
+    "forbid_segments": [
+
     ]
 }
 
-@pytest.fixture
-def setup_empty_data_file(tmp_path):
-    data_file = tmp_path / "empty_data.json"
-    with open(data_file, 'w') as f:
-        json.dump(mock_empty_data, f)
-    return data_file
+expected_matrix_3 = np.array([
+    [10 ** 8, 5.0],
+    [5.0, 10 ** 8]
+])
 
-@pytest.fixture
-def setup_single_point_file(tmp_path):
-    data_file = tmp_path / "single_point_data.json"
-    with open(data_file, 'w') as f:
-        json.dump(mock_single_point_data, f)
-    return data_file
+mock_data_4 = {
+    "points": [
+        {"x": 0.5, "y": 0},
+        {"x": 3, "y": 4.2},
+        {"x": 6.3, "y": -8}
+    ],
+    "forbid_segments": [
+        [0, 1]
+    ]
+}
 
-@pytest.fixture
-def setup_two_points_file(tmp_path):
-    data_file = tmp_path / "two_points_data.json"
-    with open(data_file, 'w') as f:
-        json.dump(mock_two_points_data, f)
-    return data_file
+expected_matrix_4 = np.array([
+    [10 ** 8, 10 ** 8, 9.881295],
+    [10 ** 8, 10 ** 8, 12.638433],
+    [9.881295, 12.638433, 10 ** 8]
+])
 
-def test_matrix_reading_empty(setup_empty_data_file):
-    distance_matrix = matrix_reading(setup_empty_data_file)
-    assert distance_matrix == 'error'
-
-def test_matrix_reading_single_point(setup_single_point_file):
-    distance_matrix = matrix_reading(setup_single_point_file)
-    expected_matrix = np.array([[10**8]])
+@pytest.mark.parametrize('mock_data, expected_matrix',
+                         [(mock_data_1, expected_matrix_1), (mock_single_point_data, expected_matrix_2),
+                          (mock_two_points_data, expected_matrix_3), (mock_data_4, expected_matrix_4)])
+def test_matrix_reading(mock_data, expected_matrix):
+    distance_matrix = matrix_distance(mock_data)
     np.testing.assert_array_almost_equal(distance_matrix, expected_matrix)
-
-def test_matrix_reading_two_points(setup_two_points_file):
-    distance_matrix = matrix_reading(setup_two_points_file)
-    expected_matrix = np.array([
-        [10**8, 5.0],
-        [5.0, 10**8]
-    ])
-    np.testing.assert_array_almost_equal(distance_matrix, expected_matrix)
-
