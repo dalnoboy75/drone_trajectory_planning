4e369ce5969bb495493dbe11218c965f32f01bc5
Ivan Novikov <novikovivan005@gmail.com>
2024-12-05 16:41:15 +0300

    add file

---
+++ program.py
@@ -0,0 +1,15 @@
+import json
+from geometry.matrix_reading import Task
+from Alg_Little.Alg_L_Classes import algorithm_Lit
+def main():
+    with open('geometry/data.json') as file:
+        dt = json.load(file)
+
+    t = Task(dt)
+    matrix = t.length_matrix
+    s = t.length_matrix.shape[0]
+    ans, answer = algorithm_Lit(matrix, s, 4)
+    print(ans, answer)
+
+if __name__ == '__main__':
+    main()
